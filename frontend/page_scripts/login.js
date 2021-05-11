c9a7d996e6a0f4f705344e90322d82c74a98f49c
cukmekerb <cukmekerb@gmail.com>
2021-05-10 17:31:14 -0700

    thought the backend was done. was wrong. hope i'm not wrong again.

--- frontend/page_scripts/login.js
+++ frontend/page_scripts/login.js
@@ -1,1 +1,37 @@
+var base_endpoint = "/.netlify/functions/cors/";
+async function login() {
+  // todo: form validation
+  var baseurl = addhttp(document.querySelector("#baseurl").value);
+  var post_data = {
+    From: "",
+    InterfaceSource: "WebApp",
+    Username: document.querySelector("#username").value,
+    Password: document.querySelector("#password").value
+  };
+  
+  // parse the response to get the token
+  var token_get = await fetch(base_endpoint + baseurl + "/app/");
+  var token_plaintext = await token_get.text();
+  var token_dom = (new DOMParser()).parseFromString(token_plaintext, "text/html");
+  var antiforge_div = token_dom.querySelector("#__AjaxAntiForgery");
+  var token = antiforge_div.firstElementChild.value;
+  console.log(token);
+  
+  
+  fetch(base_endpoint + baseurl + "/api/SignIn", {
+    method: "POST",
+    body: JSON.stringify(post_data),
+    headers: {
+      "RequestVerificationToken": token
+    }
+  });
+}
 
+// stolen from staccoverflow
+function addhttp(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "https://" + url;
+    }
+    url = url.replace(/\/$/, "");
+    return url;
+}
--- netlify_functions/cors.js
+++ netlify_functions/cors.js
@@ -1,34 +1,44 @@
-var axios = require("axios");
+var fetch = require("node-fetch");
 exports.handler = async (event, context) => {
 	var url = event.path;
-	
+
 	url = url.split(".netlify/functions/cors/")[1];
 	url = decodeURIComponent(url);
 	url = new URL(url);
 	for (var i in event.queryStringParameters) {
 		url.searchParams.append(i, event.queryStringParameters[i]);
-  }
-  console.log(url.href);
+	}
+	console.log(url.href);
 	var cookie_string = event.headers.cookie || "";
 	var useragent = event.headers["user-agent"] || "";
+	var RVC = event.headers.requestverificationtoken || "";
+	console.log(RVC)
+	var headers_to_send = {
+		"Cookie": cookie_string,
+		"User-Agent": useragent,
+		"RequestVerificationToken": RVC,
+		"content-type": "application/json",
+		"accept": "*/*",
+		"host": url.host
+	};
 	var options = {
-    url: url.href,
-    method: event.httpMethod.toUpperCase(),
-    headers: {
-    	"Cookie": cookie_string,
-    	"User-Agent": useragent
-    },
-    data: event.body,
-    responseType: "text",
-    transformResponse: [(data) => { return data; }] // prevent JSON parsing
-  };
-  var response = await axios(options);
-  return {
-  	statusCode: 200,
-  	body: response.data,
-  	headers: {
-  	  "content-type": response.headers["content-type"] || "",
-  	  "set-cookie": response.headers["set-cookie"] || ""
-  	}
-  };
+		method: event.httpMethod.toUpperCase(),
+		headers: headers_to_send,
+		body: event.body
+	};
+	var response = await fetch(url, options);
+	var response_text = await response.text();
+	var headers = response.headers.raw();
+	console.log(headers);
+	return {
+		statusCode: 200,
+		body: response_text,
+		headers: {
+			"content-type": headers["content-type"] || "",
+			"set-cookie": headers["set-cookie"] || "no-new-cooks=yes;",
+			"X-gimme-da-cooks": headers["set-cookie"] || "no-new-cooks=yes;", // looks like we're gonna have to use this lol
+			"access-control-expose-headers": "set-cookie"
+		}
+	};
 };
+
