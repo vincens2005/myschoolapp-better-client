fa36e51e4b5ec55df7b126092ab988abe4eea9e4
cukmekerb <cukmekerb@gmail.com>
2021-08-06 15:08:15 -0700

    class.html: announcemets section

--- frontend/page_scripts/class.js
+++ frontend/page_scripts/class.js
@@ -19,12 +19,11 @@ async function init() {
 
 	user.last_page = location;
 	save_data(user);
-	
+
 	var tab = url.hash.replace(/#/gi, "");
 	tab = tab.toLowerCase();
-	
-	var tabs = [
-		{
+
+	var tabs = [{
 			hash: "bboard",
 			label: "Bulletin Board",
 			class_name: ((tab == "bboard" || !tab) ? "current-tab" : "")
@@ -37,14 +36,16 @@ async function init() {
 		{
 			hash: "roster",
 			label: "Roster",
-			class_name:  ((tab == "roster") ? "current-tab" : "")
+			class_name: ((tab == "roster") ? "current-tab" : "")
 		}
 	]
-	
+
 	empty_all(document.querySelector("#class-display"))
-	fill_template("tab-template", {tabs}, "tabs");
-	
-	switch(tab) {
+	fill_template("tab-template", {
+		tabs
+	}, "tabs");
+
+	switch (tab) {
 		case "topics":
 			fetch_topics(classid);
 			break;
@@ -54,13 +55,13 @@ async function init() {
 		default:
 			fetch_bulletin(classid);
 	}
-	
-	fetch(base_endpoint + user.baseurl +`/api/datadirect/SectionInfoView/?format=json&SectionId=${classid}&associationId=1`).then(a => a.json())
-	.then(data => {
-				document.title = data[0].GroupName + " - portal++"
-				document.querySelector("#title").innerHTML = data[0].GroupName + " - " + data[0].Identifier + ` (${data[0].Block})`;
-				document.querySelector("#title").classList.remove("ohidden");
-			});
+
+	fetch(base_endpoint + user.baseurl + `/api/datadirect/SectionInfoView/?format=json&SectionId=${classid}&associationId=1`).then(a => a.json())
+		.then(data => {
+			document.title = data[0].GroupName + " - portal++"
+			document.querySelector("#title").innerHTML = data[0].GroupName + " - " + data[0].Identifier + ` (${data[0].Block})`;
+			document.querySelector("#title").classList.remove("ohidden");
+		});
 }
 
 
@@ -78,6 +79,7 @@ async function fetch_bulletin(id) {
 					links.push({
 						url: item.Url,
 						short_desc: item.ShortDescription,
+						is_link: true,
 						desc: item.Description
 					});
 				}
@@ -98,14 +100,15 @@ async function fetch_bulletin(id) {
 						url: download_endpoint + user.baseurl + item.DownloadUrl,
 						short_desc: item.Description,
 						desc: item.Description,
+						is_link: true,
 						filename: item.FileName
 					});
 				}
 				fill_template("links_template", {
-					items: downloads,
-					title: "Downloads",
-				},
-				"bulletin-sidebar");
+						items: downloads,
+						title: "Downloads",
+					},
+					"bulletin-sidebar");
 			}
 		},
 		// bulletin board text endpoint
@@ -113,25 +116,45 @@ async function fetch_bulletin(id) {
 			url: `/api/text/forsection/${id}/?format=json&contextLabelId=2`,
 			handler: data => {
 				if (!data.length) return;
-				if(!data[0].Description) return;
-				
+				if (!data[0].Description) return;
+
 				var forsection = { // what is a forsection?
 					title: data[0].Description,
 					desc: data[0].LongText
 				}; // i have no idea
-				
+
 				fill_template("main-template", forsection, "top-bulletin-sections", {
 					noEscape: true // there is no escape
 				});
 			}
 		},
+		// announcements endpoint
+		{
+			url: `/api/announcement/forsection/${id}/?format=json&active=true&future=false&expired=false&contextLabelId=2`,
+			handler: data => {
+				if (!data.length) return;
+				var announcements = [];
+				for (let item of data) {
+					announcements.push({
+						is_link: false,
+						short_desc: item.Name,
+						date: item.PublishDateDisplay
+					});
+				}
+
+				fill_template("links_template", {
+						items: announcements,
+						title: "Annoncements",
+					}, "bulletin-sidebar");
+			}
+		},
 	]
-	
+
 	for (var endpoint of endpoints) {
 		// in this case, use .then rather than await so that these happen simultaniouesutly
 		fetch(base_endpoint + user.baseurl + endpoint.url)
-		.then(a => a.json())
-		.then(endpoint.handler);
+			.then(a => a.json())
+			.then(endpoint.handler);
 	}
 	document.querySelector("#bulletin-sidebar").classList.remove("ohidden");
 }
@@ -139,10 +162,10 @@ async function fetch_bulletin(id) {
 async function fetch_roster(id) {
 	var ftp_image_path = await get_image_path();
 	console.log(ftp_image_path)
-	
+
 	var roster = await fetch(base_endpoint + user.baseurl + `/api/datadirect/sectionrosterget/${id}/?format=json`).then(a => a.json());
 	var people = [];
-	
+
 	for (var person of roster) {
 		people.push({
 			name: person.name,
@@ -150,13 +173,15 @@ async function fetch_roster(id) {
 			url: "#roster" // TODO: profile page
 		})
 	}
-	
-	fill_template("roster-template", {people}, "roster-container");
+
+	fill_template("roster-template", {
+		people
+	}, "roster-container");
 }
 
 async function fetch_topics(id) {
 	var ftp_image_path = await get_image_path();
-	
+
 	var topics_raw = await fetch(base_endpoint + user.baseurl + `/api/datadirect/sectiontopicsget/${id}/?format=json&active=true&future=false&expired=false&sharedTopics=false`).then(a => a.json());
 	var topics = [];
 	for (var topic of topics_raw) {
@@ -166,7 +191,9 @@ async function fetch_topics(id) {
 			url: "topic.html?topic=" + topic.TopicID
 		});
 	}
-	
+
 	// use the same template as the roster because it's basically the same
-	fill_template("roster-template", {people: topics}, "roster-container");
+	fill_template("roster-template", {
+		people: topics
+	}, "roster-container");
 }
